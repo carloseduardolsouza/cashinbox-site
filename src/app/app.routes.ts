6e95db8201bb80f654135c19f2d6fc61b7738917
Carlos Eduardo Souza <carlosreiroyale@gmail.com>
2025-12-19 12:46:40 -0300

    criação do header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './screens/home/home';
+import { Cadastro } from './screens/cadastro/cadastro';
+import { Login } from './screens/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: '/cadastro',
+    component: Cadastro,
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+];
---
+++ src/app/screens/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavHeader } from '../../components/nav-header/nav-header';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavHeader],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
