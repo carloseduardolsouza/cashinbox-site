3ee5d4cc804d25fe523c53b561b2199f813280bd
Carlos Eduardo Souza <carlosreiroyale@gmail.com>
2025-12-30 16:37:38 -0300

    criação da pagina home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './screens/home/home';
-import { Cadastro } from './screens/cadastro/cadastro';
-import { Login } from './screens/login/login';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
-  },
-  {
-    path: '/cadastro',
-    component: Cadastro,
-  },
-  {
-    path: '/login',
-    component: Login,
   },
 ];
